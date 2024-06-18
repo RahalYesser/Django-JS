7f007db6b665597d9775af781c1d44bca3370455
unknown <rahalyesser9@gmail.com>
2024-06-18 04:13:41 +0200

    postgreSQL

--- backend/PI/PI/settings.py
+++ backend/PI/PI/settings.py
@@ -93,13 +93,23 @@ WSGI_APPLICATION = 'PI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+} """
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'work',
+        'USER': 'postgres',
+        'PASSWORD':'',
+        'Host':'127.0.0.1',
+        'PORT':5432
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
